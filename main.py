83b7158db73dd0080a9ad697302541bc0578d90a
marutibhosale <59178777+marutibhosale@users.noreply.github.com>
2020-11-26 14:27:52 +0530

    Add: connect clinet with server

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ socketio = SocketIO(app)
 def home():
     return render_template("home.html")
 
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template('chat.html', username=username, room=room)
+    else:
+        return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
