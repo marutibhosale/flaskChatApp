49755007864208229ddb4865af3dedccd18a3837
marutibhosale <59178777+marutibhosale@users.noreply.github.com>
2020-11-26 16:31:55 +0530

    Add: leave room and leave room announcement

--- main.py
+++ main.py
@@ -34,6 +34,12 @@ def handle_send_message_event(data):
                                                                     data['message']))
     socketio.emit('receive_message', data, room=data['room'])
 
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    print("{} has left the room {}".format(data['username'], data['room']))
+    leave_room(data['room'])
+    socketio.emit('leave_room_announcement', data, room=data['room'])
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
